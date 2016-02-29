3d26fc56e7e25f4997b2578cb6aca4460903cfe7
Rupinder Singh Khokhar <rsk1coder99@gmail.com>
2016-02-29 21:18:36 +0530

    new way

---
+++ exp5/applet/1.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+  <form action="" >
+    <input type="checkbox" name="vehicle" value="000"> 000<br>
+    <input type="checkbox" name="vehicle" value="001"> 001<br>
+    <input type="checkbox" name="vehicle" value="010" checked> 010<br>
+    <input type="checkbox" name="vehicle" value="011" checked> 011<br>
+    <input type="checkbox" name="vehicle" value="100"> 100<br>
+    <input type="checkbox" name="vehicle" value="101"> 101<br>
+    <input type="checkbox" name="vehicle" value="110"> 110<br>
+    <input type="checkbox" name="vehicle" value="111"> 111<br>
+    <input type="submit" value="Submit">
+  </form> 
+</body>
+</html>
+<?php
+echo $_GET;
+?>
